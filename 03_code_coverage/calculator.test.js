25e440d73937b2be5d77ab5c556b9c106067cc48
Artem Govorov <artem.govorov@gmail.com>
2021-07-14 14:25:35 +1000

    Module 03 WIP

--- 03_code_coverage/calculator.test.js
+++ 03_code_coverage/calculator.test.js
@@ -2,11 +2,23 @@ class Calculator {
   add(a, b) {
     return a + b;
   }
+
+  multiply(a, b) {
+    if (a > 0 && b > 0) return a * b;
+    return 0;
+  }
 }
 
 describe('calculator', () => {
   it('adds numbers', () => {
     const calculator = new Calculator();
     // expect(calculator.add(1, 2)).toBe(3);
+  });
+
+  it('multiplies numbers', () => {
+    const calculator = new Calculator();
+    expect(calculator.multiply(0, 2)).toBe(0);
+    // expect(calculator.multiply(2, 0)).toBe(0);
+    // expect(calculator.multiply(2, 2)).toBe(4);
   });
 });
