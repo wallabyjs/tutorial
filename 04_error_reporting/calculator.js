dd8f035a6255684f8326cc5ca9454fb3fdd1634d
Artem Govorov <artem.govorov@gmail.com>
2021-07-15 11:52:45 +1000

    Module 04 WIP

---
+++ 04_error_reporting/calculator.js
@@ -0,0 +1,7 @@
+export function add(a, b) {
+  return x + y;
+}
+
+export function multiply(a, b) {
+  return a + b;
+}
---
+++ 04_error_reporting/calculator.test.js
@@ -0,0 +1,9 @@
+import {add, multiply} from './calculator'
+
+it('adds numbers', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+it('multiplies numbers', () => {
+  expect(multiply(3, 2)).toBe(6);
+});
