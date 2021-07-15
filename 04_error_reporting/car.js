1924b1973de1e1355be46a7f612b83b67c9f135f
Artem Govorov <artem.govorov@gmail.com>
2021-07-15 15:45:08 +1000

    Module 04 WIP

---
+++ 04_error_reporting/car.js
@@ -0,0 +1,11 @@
+export default class Car {
+  constructor(initialSpeed) {
+    this.engine = '2.85 L';
+    this.power = '97 kW';
+    this.speed = initialSpeed;
+  }
+
+  accelerate() {
+    this.speed++;
+  }
+}
---
+++ 04_error_reporting/car.test.js
@@ -0,0 +1,19 @@
+import Car from './car';
+
+let car;
+
+describe('car', () => {
+
+  beforeEach(() => {
+    car = new Car(88);
+  });
+
+  it('has correct engine', () => {
+    expect(car.engine).toBe('2.94 L');
+  });
+
+  it('accelerates', () => {
+    car.accelerate();
+    expect(car).toMatchSnapshot();
+  });
+});
