3ac24916d68c11eecfd0c0ec6cb080485368874e
Artem Govorov <artem.govorov@gmail.com>
2021-07-23 13:26:18 +1000

    Module 05 WIP

--- 05_runtime_values/car.js
+++ 05_runtime_values/car.js
@@ -2,6 +2,13 @@ export default class Car {
   constructor(initialSpeed) {
     this.engine = '2.85 L';
     this.power = '97 kW';
+    this.assembly = 'DeLorean Motor Cars, Ltd.';
+    this.transmission = {
+      type: 'manual',
+      speed: 5
+    };
+    this.length = 4267;
+    this.width = 1988;
     this.speed = initialSpeed;
   }
 
