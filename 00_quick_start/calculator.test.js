4ba0adea1cc7b6e45151dff4577190a0350eef16
Artem Govorov <artem.govorov@gmail.com>
2025-02-21 17:05:39 +1000

    chore: update calculator.test.js

--- 00_quick_start/calculator.test.js
+++ 00_quick_start/calculator.test.js
@@ -1,15 +1,28 @@
 const Calculator = require('./calculator');
 
+// Check out the comments below 👇
+
 describe('calculator', () => {
   it('adds numbers', () => {
     const calculator = new Calculator();
     expect(calculator.add(1, 2)).toBe(3);
+    // with live comment see inline value change in real-time as you update the code
     calculator.add(100, 200) //?
   });
 
   it('multiplies numbers', () => {
     const calculator = new Calculator(); //?
-    // fix me!
+    // Let's fix the error, try clicking Debug code lens on the test
     expect(calculator.multiply(2, 2)).toBe(5);
   });
 });
+
+/*
+  Here are some more cool things you can quickly try:
+  - 🐛 Time Travel Debugger    https://wallabyjs.com/docs/features/time-travel-debugger/
+  - 🔎 Value Peek              https://wallabyjs.com/docs/features/advanced-logging/#value-peek 
+  - 🔴 Logpoints               https://wallabyjs.com/docs/features/advanced-logging/#logpoints
+  - 🤖 GitHub Copilot Support  https://wallabyjs.com/docs/features/copilot/
+
+  - 📖 Detailed Wallaby Intro  https://wallabyjs.com/docs/intro/vs-code/
+*/
