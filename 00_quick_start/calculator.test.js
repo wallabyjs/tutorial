2a7dc4697ba3047e61f6284eef6f6f1870ab51ae
Artem Govorov <artem.govorov@gmail.com>
2025-02-21 16:51:52 +1000

    chore: update calculator.test.js

--- 00_quick_start/calculator.test.js
+++ 00_quick_start/calculator.test.js
@@ -1,13 +1,4 @@
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
-
-  multiply(a, b) {
-    if (a > 0 && b > 0) return a * b;
-    return 0;
-  }
-}
+const Calculator = require('./calculator');
 
 describe('calculator', () => {
   it('adds numbers', () => {
@@ -17,9 +8,8 @@ describe('calculator', () => {
   });
 
   it('multiplies numbers', () => {
-    const calculator = new Calculator();
-    expect(calculator.multiply(0, 2)).toBe(0);
-    expect(calculator.multiply(2, 0)).toBe(0);
-    expect(calculator.multiply(2, 2)).toBe(4);
+    const calculator = new Calculator(); //?
+    // fix me!
+    expect(calculator.multiply(2, 2)).toBe(5);
   });
 });
